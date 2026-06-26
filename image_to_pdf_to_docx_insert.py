0ee7b298543210e7fb202046be8f522051398f9b
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-26 13:48:32 -0300

    Update image_to_pdf_to_docx_insert.py

--- image_to_pdf_to_docx_insert.py
+++ image_to_pdf_to_docx_insert.py
@@ -253,8 +253,7 @@ def saveMultImgDocx():
     doc = Document()
     section = doc.sections[0]
     section.page_width = Inches(sizePaper[0])
-    section.page_height = Inches(sizePaper[1])
-    doc.add_heading('Imagens Inseridas', level=1)
+    section.page_height = Inches(sizePaper[1])    
     nBts = len(uploades)
     for u, upload in enumerate(uploades):
         bytesData = st.session_state['bytesAll'][keysData[u]][0]
